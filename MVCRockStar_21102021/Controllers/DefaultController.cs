5fad267fedaf96157c5a80b8e6a265442e77a954
yawar2019 <yawarali17@gmail.com>
2021-10-28 09:12:38 +0530

    committed

--- MVCRockStar_21102021/Controllers/DefaultController.cs
+++ MVCRockStar_21102021/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCRockStar_21102021.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -46,6 +47,58 @@ namespace MVCRockStar_21102021.Controllers
         public string testTime3(int? id)
         {
             return "hello " + id + " My Name is " + Request.QueryString["Name"]+" Designation " + Request.QueryString["Designation"];
+        }
+
+        //[NonAction]
+        private string GetEmployeeInfo()
+        {
+            
+            ViewBag.EmployeeName = "Preeti";
+            return "preeti";
+        }
+
+        public ActionResult GetEmployeeInfo2()
+        {
+            string Name = GetEmployeeInfo();
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = Name;
+            obj.EmpSalary = 21000;
+
+            ViewBag.Employee = obj;
+
+            return View();
+        }
+
+        public ActionResult GetEmployeeInfo3()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Preeti";
+            obj.EmpSalary = 21000;
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Divya";
+            obj1.EmpSalary = 22000;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Tajun";
+            obj2.EmpSalary = 23000;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+
+            ViewBag.Employee = listObj;
+
+            return View();
         }
     }
 }
