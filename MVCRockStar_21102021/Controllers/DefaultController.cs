6e7403575490e94d277409aab3e91ce64c006a26
yawar2019 <yawarali17@gmail.com>
2021-11-01 08:55:09 +0530

    committed

--- MVCRockStar_21102021/Controllers/DefaultController.cs
+++ MVCRockStar_21102021/Controllers/DefaultController.cs
@@ -100,5 +100,86 @@ namespace MVCRockStar_21102021.Controllers
 
             return View();
         }
+
+        public ActionResult MethodByViewModel()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Preeti";
+            obj.EmpSalary = 21000;
+
+            return View(obj);
+        }
+
+        public ActionResult GetEmployeeInfo4()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Preeti";
+            obj.EmpSalary = 21000;
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Divya";
+            obj1.EmpSalary = 22000;
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Tajun";
+            obj2.EmpSalary = 23000;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+            
+            return View(listObj);
+        }
+
+        public ActionResult GetEmployeeInfo5()
+        {
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Preeti";
+            obj.EmpSalary = 21000;
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Divya";
+            obj1.EmpSalary = 22000;
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Tajun";
+            obj2.EmpSalary = 23000;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+            ///End of Employee
+            ///
+            DepartmentModel dept = new Models.DepartmentModel();
+            dept.DeptId = 1;
+            dept.DeptName = "IT";
+
+            DepartmentModel dept1 = new Models.DepartmentModel();
+            dept1.DeptId = 2;
+            dept1.DeptName = "Network";
+
+            List<DepartmentModel> listDept = new List<DepartmentModel>();
+            listDept.Add(dept);
+            listDept.Add(dept1);
+            //End of Dept
+
+            EmpDept empdept = new Models.EmpDept();
+            empdept.Emp = listObj;
+            empdept.dept = listDept;
+
+            return View(empdept);
+        }
     }
 }
---
+++ MVCRockStar_21102021/Models/DepartmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRockStar_21102021.Models
+{
+    public class DepartmentModel
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    }
+
+    public class EmpDept
+    {
+        public List<EmployeeModel> Emp { get; set; }
+        public List<DepartmentModel> dept { get; set; }
+    }
+}
