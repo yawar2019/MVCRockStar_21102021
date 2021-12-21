815ab0d742714173610e081c6dc9f62efb8d372c
yawar2019 <yawarali17@gmail.com>
2021-12-21 09:06:37 +0530

    committed

---
+++ HandleErrorExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HandleErrorExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(int id)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HandleError(View ="MyFavoriteError")]
+        public ActionResult HandleExceptionExample(string id)
+        {
+            int a = 10; int b = Convert.ToInt32(id);
+            int c = (b/a);
+            return Content(c.ToString());
+        }
+    }
+}
